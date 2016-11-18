771cfb4e0f3c3a03af6548380ea4e6aa19e53c62
ChunKitChung <chung@arara.com>
2016-11-18 17:37:10 +0900

    api practice finished

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask, request, render_template
+# ----------------------
+# web framework
+# ----------------------
+from flask import Flask, request, render_template, jsonify, json
+
+
+# ----------------------
+# DB
+# ----------------------
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+# ----------------------
+# Models
+# ----------------------
+from models import Base, Puppy
+
+
 
 
 # ----------------------
 # app configs
 # ----------------------
+
+engine = create_engine('sqlite:///puppies.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 app = Flask(__name__)
 
@@ -14,58 +38,125 @@ app = Flask(__name__)
 
 
 # ----------------------
-# end points
+# end points: puppies
 # ----------------------
 
 @app.route("/")
 def home():
     return render_template("home.html", name="home")
 
+
 @app.route("/puppies", methods = ['GET', 'POST'])
+def puppies_landing_page():
+
+    message = None
+
+    if request.method == 'GET':
+        message = "Getting All the puppies!"
+    elif request.method == 'POST':
+        message = "Creating A New Puppy!"
+
+    return render_template("puppies.html", message=message)
+
+@app.route("/api/puppies", methods = ['GET', 'POST'])
 def puppiesFunction():
 
-  message=None
+    data_jsonify = None
 
-  if request.method == 'GET':
-    message = getAllPuppies()
-  elif request.method == 'POST':
-    message = makeANewPuppy()
+    if request.method == 'GET':
+        data_jsonify = getAllPuppies()
+    
+    elif request.method == 'POST':
 
-  return render_template("puppies.html", message=message)
+        name = request.args.get('name', '')
+        description = request.args.get('description', '')
+        if not name:
+            name = request.form.get('name', '')
+        if not description:
+            description = request.form.get('description', '')
+
+        data_jsonify = makeANewPuppy(name, description)
+
+    return data_jsonify
+    #return render_template("puppies.html", message=message, data_json=data_json)
 
 
-@app.route("/puppies/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
+@app.route("/api/puppies/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
 def puppiesFunctionId(id):
+
     if request.method == 'GET':
         return getPuppy(id)
+
     if request.method == 'PUT':
-        return updatePuppy(id)
+        name = request.args.get('name', '', type=str)
+        description = request.args.get('description', '', type=str)
+        if not name:
+            name = request.form.get('name', '')
+        if not description:
+            description = request.form.get('description', '')
+        return updatePuppy(id, name, description)
+
     elif request.method == 'DELETE':
         return deletePuppy(id) 
 
 
+# ----------------------
+# end points: number
+# ----------------------
+
+@app.route('/number')
+def number_landing_page():
+    return render_template("number.html")
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+
 
 
 
 
 # ----------------------
-# get, make, put, delete methods
+# Puppy methods: get, make, put, delete 
 # ----------------------
 
 def getAllPuppies():
-    return "Getting All the puppies!"
+    puppies = session.query(Puppy).all()
+    return jsonify(puppies=[i.serialize for i in puppies])
   
-def makeANewPuppy():
-    return "Creating A New Puppy!"
+def makeANewPuppy(name, description):
+    puppy = Puppy(name=name, description=description)
+    session.add(puppy)
+    session.commit()
+    return jsonify(puppy=puppy.serialize) 
 
 def getPuppy(id):
-  return "Getting Puppy with id %s" % id
+    puppy = session.query(Puppy).filter_by(id=id).one()
+    return jsonify(puppy=puppy.serialize)
   
-def updatePuppy(id):
-    return "Updating a Puppy with id %s" % id
+def updatePuppy(id, name, description):
+    puppy = session.query(Puppy).filter_by(id=id).one()
+
+    if name:
+        puppy.name = name
+    if description:
+        puppy.description = description
+
+    session.add(puppy)
+    session.commit()
+
+    return jsonify(message="Updating a Puppy with id %s" % id) 
 
 def deletePuppy(id):
-    return "Removing Puppy with id %s" % id
+    puppy = session.query(Puppy).filter_by(id=id).one()
+
+    session.delete(puppy)
+    session.commit()
+
+    return jsonify(message="Removing Puppy with id %s" % id)
+
 
 
 
@@ -77,7 +168,6 @@ def deletePuppy(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
 
 
 
