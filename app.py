cc8cdf83eaba852103b34edd2f827d049b2c5132
ray chung <ray chung>
2017-01-03 16:36:11 +0900

    user model added

--- app.py
+++ app.py
@@ -48,35 +48,6 @@ app = Flask(__name__)
 def home():
     return render_template("home.html", name="home")
 
-# ----------------------
-# end points: user
-# ----------------------
-@app.route("/users", method = ['GET'])
-def user_landing_page():
-    return "user landing page"
-
-@app.route("/api/users", method = ['POST'])
-def user_landing_page():
-    username = request.json.get('username')
-    password = request.json.get('password')
-    if username is None or password is None:
-        abort(400) # missing args
-    if user_session.query(User).filter_by(username=username).first() is not None:
-        abort(400) # existing user
-    user = User(username = username)
-    user.hash_password(password)
-    user_session.add(user)
-    user_session.commit()
-    return jsonify({'username':user.username}), 201, {'Location': url_for('get_user', id=user.id, _external = True)}
-
-@app.route('/api/users/<int:id>')
-def get_user(id):
-    user = session.query(User).filter_by(id=id).one()
-    if not user:
-        abort(400)
-    return jsonify({'username':user.username})
-
-
 
 # ----------------------
 # end points: puppy
