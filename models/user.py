3f745b46184998263eb2cc3cab636ba653100182
ray chung <ray chung>
2017-01-04 04:11:02 +0900

    user template bug fixed

--- models/user.py
+++ models/user.py
@@ -7,15 +7,15 @@ from passlib.apps import custom_app_context as pwd_context
 Base = declarative_base()
 class User(Base):
 
-	# define tables
-	__tablename__ = 'user'
+    # define tables
+    __tablename__ = 'user'
 
-	# define columns
-	id = Column(Integer, primary_key=True)
-	username = Column(String(32), index=True)
-	password_hash = Column(String(64))
+    # define columns
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    password_hash = Column(String(64))
 
-	# define query response: equivalent to getter setter
+    # define query response: equivalent to getter setter
     @property
     def serialize(self):
         """Return object data in easily serializeable format"""
@@ -24,11 +24,11 @@ class User(Base):
             'username': self.username
         }
 
-	def hash_password(self, password):
-		self.password_hash = pwd_context.encrypt(password)
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
 
-	def verify_password(self, password):
-		return pwd_context.verify(password, self.password_hash)
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
 
 
 
