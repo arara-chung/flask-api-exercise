7b6f2af2502de9f81917609ba32f1b6bacb251a0
ray chung <ray chung>
2017-01-03 17:14:15 +0900

    user template added

--- app.py
+++ app.py
@@ -53,7 +53,9 @@ def home():
 # ----------------------
 @app.route("/users", methods = ['GET'])
 def user_landing_page():
-    return "user landing page"
+    return render_template("users.html", 
+        name="home",
+        message="welcome to my user landing page")
 
 @app.route("/api/users", methods = ['POST'])
 def create_user():
